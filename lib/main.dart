61dbe64dc8de5a47e54366326b37b2901c837afe
nakangmin <139530542+KangminNa@users.noreply.github.com>
2023-07-14 17:33:58 +0900

    init : pomodoro app
    - 노마트코더 강의 중 #5강의 시작
    - ThemeData 값만 넣고 1:21분부터 다시 시작해야함.
    - 빈 레이아웃에서 시작 할 예정
    - 곧 퇴근! 이여서 여기까지만

--- lib/main.dart
+++ lib/main.dart
@@ -12,80 +12,19 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
-  void onClicked() {
-    setState(() {
-      counter = counter + 1;
-    }); //State 클래스에게 데이터가 변경되었다고 알리는 함수
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xff232b55),
           ),
         ),
+        cardColor: const Color(0xfff4eddb),
       ),
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeText() : const Text('nothing'),
-              IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye))
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeText extends StatefulWidget {
-  const MyLargeText({
-    super.key,
-  });
-
-  @override
-  State<MyLargeText> createState() => _MyLargeTextState();
-}
-
-class _MyLargeTextState extends State<MyLargeText> {
-  int count = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  } // 부모요소에 의존하는 데이터를 초기화해야 하는 경우에 사용, 대부분 위 방식으로 사용.
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  } // 위젯이 스크린에서 제거될 때 호출되는 메서드,
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Click Count',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
+      home: const Scaffold(
+        body: Center(),
       ),
     );
   }
