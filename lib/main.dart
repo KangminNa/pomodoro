d3b7d7a55dfd922e8eacc0d53c364ef2c3eb2c9f
nakangmin <139530542+KangminNa@users.noreply.github.com>
2023-07-14 16:24:57 +0900

    feat : StateFulWidgets - State
    - 노마드코더의 'Flutter로 웹툰 앱 만들기' 강의 중 #4.0
    - 대표적인 두가지 위젯 중 하나인 StateFulWidget 예시
    - 현재 IconButton 클릭 시 counter값이 증가 하는 기능을 구현
    - 하지만 화면상에 counter은 증가하지 않음

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,53 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
